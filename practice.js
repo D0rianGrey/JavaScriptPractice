72295fbc441851ce11dda4c630820d1d4162cc78
eugene <utyfdfrthby08@gmail.com>
2020-03-08 21:41:29 +0200

    from pc ts

--- practice.js
+++ practice.js
@@ -1,9 +1,47 @@
-"use strict";
-function func1() {
-  console.log("From func 1");
+function getName() {
+  console.log(this.name);
 }
 
-function func2() {
-  console.log("From func 2");
-}
+const prod = {
+  name: "Eugene",
+  getName,
+  surname: "Vakerin",
+  func1: function() {
+    console.log(this);
+  },
+  info: function() {
+    console.log(this);
+  },
+  info2: {
+    insideInfo2: ["2.3ghz"],
+    func3: function() {
+      console.log(this);
+    },
+  },
+};
 
+const prod2 = {
+  name: "Karina",
+  surname: "Beautiful",
+  getSurname() {
+    console.log(this.surname);
+  },
+  func1: function() {
+    console.log(this);
+  },
+  info: function() {
+    console.log(this);
+  },
+  info2: {
+    insideInfo2: ["2.3ghz"],
+    func3: function() {
+      console.log(this);
+    },
+  },
+};
+
+prod.getName();
+prod2.getSurname();
+
+prod2.getName = prod.getName;
+prod2.getName();
