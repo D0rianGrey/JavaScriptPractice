784df5f1e743021d325006dfed08aa35cf9305d6
eugene <utyfdfrthby08@gmail.com>
2020-03-08 22:44:28 +0200

    from pc

--- practice.js
+++ practice.js
@@ -1,10 +1,20 @@
 function getName() {
   console.log(this.name);
+  return this;
 }
+
+function getCountry() {
+  console.log(this.country);
+  return this;
+}
+
+
 
 const prod = {
   name: "Eugene",
+  country:"Ukraine",
   getName,
+  getCountry,
   surname: "Vakerin",
   func1: function() {
     console.log(this);
@@ -22,6 +32,7 @@ const prod = {
 
 const prod2 = {
   name: "Karina",
+  country:"Germany",
   surname: "Beautiful",
   getSurname() {
     console.log(this.surname);
@@ -40,8 +51,12 @@ const prod2 = {
   },
 };
 
-prod.getName();
-prod2.getSurname();
+// prod.getName();
+// prod2.getSurname();
 
-prod2.getName = prod.getName;
-prod2.getName();
+// prod2.getName = prod.getName;
+// prod2.getName();
+
+prod.getName().getCountry();
+getName.call(prod2);
+
